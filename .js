b46ef0954f8a44ab933e7462b790f9c701facf54
TheBesiak <97199178+TheBesiak@users.noreply.github.com>
2024-02-03 12:09:23 +0100

    Update .js

--- .js
+++ .js
@@ -1,8 +1,1 @@
-// Sprawdź, czy ciasteczko zostało już ustawione
-if (document.cookie.indexOf('alertShown=true') === -1) {
-  // Jeśli nie, wyświetl alert
-  window.alert("MargoVanish by Besiak - W razie problemów pisać na dc: besiak06");
 
-  // Ustaw ciasteczko, że alert został już wyświetlony
-  document.cookie = 'alertShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
-}
